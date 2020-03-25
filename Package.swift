37019de25c5a42e406a8726a364c9db176c080bd
Clément Leroy <clement.leroy62@gmail.com>
2020-03-25 15:13:40 +0100

    fix spm

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "Tabman",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v10)
     ],
     products: [
         .library(
@@ -13,20 +13,13 @@ let package = Package(
             targets: ["Tabman"])
     ],
     dependencies: [
-        .package(url: "https://github.com/uias/Pageboy", from: "3.5.0")
+        .package(url: "https://github.com/clementleroy/Pageboy", from: "3.5.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Tabman",
-            dependencies: ["Pageboy"],
-            path: ".",
-            sources: ["Sources/Tabman"]),
-        .testTarget(
-            name: "TabmanTests",
-            dependencies: ["Tabman"]
-        )
-    ],
-    swiftLanguageVersions: [.v4, .v4_2, .v5]
+            dependencies: ["Pageboy"])
+    ]
 )
